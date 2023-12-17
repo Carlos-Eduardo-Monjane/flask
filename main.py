e48c749b00c2cf3a17eddac7edb0ca5e54460a9a
Carlos Eduardo Monjane <carlosmonjane98@gmail.com>
2023-12-17 20:52:38 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,6 @@
 from flask import Flask, render_template, request, jsonify
-import requests
-from bardapi import SESSION_HEADERS
-from bardapi import Bard
-
 
 app = Flask(__name__)
-token = "eQgaoLKfSx5xmduYq_HwnXYOJCB4Tr1XalQq32s-zqMJG96aeB_GqNC24JTyjrBVmG60PQ."
-
-session = requests.Session()
-session.headers = SESSION_HEADERS
-session.cookies.set("__Secure-1PSID", token)
-session.cookies.set("__Secure-1PSIDTS","sidts-CjIBPVxjSlo377k3v2NAsN2M6g-XXbqziSbyDFGDpzHocuxX43wIGtxCP1-7q4u-tPQLKRAA")
-session.cookies.set("__Secure-1PSIDCC","ABTWhQHSxdksLdP6b5Af0gLU3WMbo7ZzC5fGRTUC-8wNaM90vzmKvhNFIydZ-6aqCturZy5oQfg")
-
-
-
-# print(bard.get_answer("quem é o presidente de brasil")['content'])
 
 @app.route('/')
 def index():
@@ -27,8 +12,23 @@ def hello_world():
 
 @app.route('/perguntar', methods=['POST'])
 def post():
-    data = request.get_json()
+    import requests
+    from bardapi import SESSION_HEADERS
+    from bardapi import Bard
+
+    token = "eQgaoLKfSx5xmduYq_HwnXYOJCB4Tr1XalQq32s-zqMJG96aeB_GqNC24JTyjrBVmG60PQ."
+
+    session = requests.Session()
+    session.headers = SESSION_HEADERS
+    session.cookies.set("__Secure-1PSID", token)
+    session.cookies.set("__Secure-1PSIDTS","sidts-CjIBPVxjSlo377k3v2NAsN2M6g-XXbqziSbyDFGDpzHocuxX43wIGtxCP1-7q4u-tPQLKRAA")
+    session.cookies.set("__Secure-1PSIDCC","ABTWhQHSxdksLdP6b5Af0gLU3WMbo7ZzC5fGRTUC-8wNaM90vzmKvhNFIydZ-6aqCturZy5oQfg")
+
     bard = Bard(token=token, session=session)
+
+
+  
+    data = request.get_json()
     resposta = bard.get_answer("quem é o presidente de brasil")['content']
     return jsonify({'message': resposta})
 
