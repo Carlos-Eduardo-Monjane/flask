9b7248c88db97970fbe44eccc3040121931d3997
Carlos Eduardo Monjane <carlosmonjane98@gmail.com>
2023-12-17 19:41:34 +0200

    Update main.py

--- main.py
+++ main.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index():
   return render_template('index.html')
 
+@app.route('/teste', methods=['GET'])
+def hello_world():
+    return jsonify({'message': Olá mazza charles!'})
+
 if __name__ == '__main__':
   app.run(port=5000)
