d01b4e2b5a73dea656ba4fd33c491899ca39f234
afnhampossa <afnhampossa15@gmail.com>
2023-12-28 10:31:31 +0100

    keys

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template, request, jsonify
-from flask_cors import CORS
-from flask_cors import cross_origin 
+from flask_cors import CORS 
 
 app = Flask(__name__)
 CORS(app)
@@ -14,19 +13,18 @@ def hello_world():
     return jsonify({'message': 'Olá mazza charles!'})
 
 @app.route('/perguntar', methods=['POST'])
-@cross_origin(origins=['http://localhost:8080'], supports_credentials=True)
 def post():
     import requests
     from bardapi import SESSION_HEADERS
     from bardapi import Bard
 
-    token = "eQgaoECt4ON9dOEL3GHfvhV-TuYRcj9959E4blRe_OljLT176rHJEWoJFHSUvPM6g4WRqg."
+    token = "egiJeRmaQE-NGT0MgBi2CYj_mVmRm62Vxa5svbjJm1NZYlm6zFgMhuLF5UmjFMIlSuvMVA."
     
     session = requests.Session()
     session.headers = SESSION_HEADERS
     session.cookies.set("__Secure-1PSID", token)
-    session.cookies.set("__Secure-1PSIDTS","sidts-CjIBPVxjSn7EowYUg3qMrVOGPIDPMW5cS5h-VawpT0N_j4cShpSujFgymmdBX3zCCLBW6hAA")
-    session.cookies.set("__Secure-1PSIDCC","ABTWhQFJ3e3OPDf_L0FZNeI344IVwEacvBwQpeLijwbFz1MqusuN3KfRj-ueaXJTn2ZOm11Bwnk")
+    session.cookies.set("__Secure-1PSIDTS","sidts-CjIBPVxjSufyxh6DwguxLkkNj1bDAfLzXGdg0UDrE9-btvSmL939wPH5RMVXbTiQyn4cWRAA")
+    session.cookies.set("__Secure-1PSIDCC","ABTWhQG-SOCttrXiTUBakFE_XUwwvXV3mmo77RZ5gJSWjapJQ54-WyXLqf2LEapesVwy2QM_Qg")
 
     bard = Bard(token=token, session=session)
 
