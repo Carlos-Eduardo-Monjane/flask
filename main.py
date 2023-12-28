99bfa3c8b1ab9bc14f05ab0af9cf000675e545f3
Angelino Fernando Nhampossa <77185331+afnhampossa@users.noreply.github.com>
2023-12-28 10:31:37 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, request, jsonify
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "*"}})
 
 @app.route('/')
 def index():
