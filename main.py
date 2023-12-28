9ce3fa82046b0a80cfb8b0b4b017f540e8b49f96
Angelino Fernando Nhampossa <77185331+afnhampossa@users.noreply.github.com>
2023-12-28 09:27:29 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template, request, jsonify
-from flask_cors import CORS
-from flask_cors import cross_origin 
+from flask_cors import CORS 
 
 app = Flask(__name__)
 CORS(app)
